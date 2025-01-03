432289e0234b99118a1c8ca97ba9f45166796abf
FES001 <firdavs7794948@gmail.com>
2025-01-03 20:26:53 +0500

    Working on it.

---
+++ scripts/search.js
@@ -0,0 +1,4 @@
+const logosInputItself = document.getElementById('logosInputItself');
+const searchButton = document.getElementById('searchButton');
+const errorParagraph = document.querySelector('.logos-input-paragraph');
+const logosOutput = document.querySelector('.logos-output');
